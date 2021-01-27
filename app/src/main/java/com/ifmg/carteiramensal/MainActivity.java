cc4dd2c990259df344d1699be3ee6c00780df661
IsaAlane <isamelo.d@gmail.com>
2021-01-27 19:07:34 -0300

    parte 4 - até aula 40 completa aplicativo ainda não executa

--- app/src/main/java/com/ifmg/carteiramensal/MainActivity.java
+++ app/src/main/java/com/ifmg/carteiramensal/MainActivity.java
@@ -1,8 +1,12 @@
 package com.ifmg.carteiramensal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -127,6 +131,18 @@ public class MainActivity extends AppCompatActivity {
 
         mostraDataApp();
         atualizaValores();
+        configuraPermissoes();
+    }
+
+    private void configuraPermissoes(){
+        if(ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
+        || ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(getBaseContext(),
+                Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                            Manifest.permission.READ_EXTERNAL_STORAGE}, 0);
+        }
     }
 
     private void atualizaValores(){
